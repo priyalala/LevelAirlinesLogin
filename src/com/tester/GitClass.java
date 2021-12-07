238b1f27f27f767199afbb519a9d0e9755d560b8
priyalala <jayapriya440@gmail.com>
2021-12-07 21:05:26 +0530

    userName Functionality Compelted

--- src/com/tester/GitClass.java
+++ src/com/tester/GitClass.java
@@ -2,4 +2,11 @@ package com.tester;
 
 public class GitClass {
 
+	public static void username() {
+System.out.println("User name ");
+}
+	
+	public static void main(String[] args) {
+		username();
+	}
 }
